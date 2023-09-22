026d67770e24cdbea12975cfa1ef123bb8655e66
AslanovIK <aslanov.pir@gmail.com>
2023-09-22 17:37:12 +0300

    Доработка 3 лабы и выполнение 4

--- 3randArray/randArray.js
+++ 3randArray/randArray.js
@@ -1,20 +1,26 @@
 import { renderResponse } from "../main/script/functions.js";
 
 let arrayWithRandom = [];
-function generateRandom(arg, arg2) {
-  return Math.floor(arg2 - Math.random() * arg);
+const testRange = [-1, -0.5, 0, 0.5, 1, 1.5, 2, 2.5, 3];
+function generateRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
-function complexArray() {
-  for (let i = 0; i < 10; i++) {
-    let temp = generateRandom(10, -10);
-    console.log(temp);
-  }
+function complexArray(arr) {
+  arr = [];
+  testRange.forEach((item) => {
+    if (item < 0) arr.push(Math.pow(item, 2));
+    else if (item === 0) arr.push(item);
+    else if (item > 0 && item <= 1) arr.push(Math.pow(item, 3));
+    else arr.push(Math.log(item).toFixed(3));
+  });
+  renderResponse(
+    `<div class="victory">[Сложная функция] ${arr.join(" | ")} </div>`,
+  );
 }
-complexArray();
 function fillArrayWithPositive(arr) {
   for (let i = 0; i < 10; i++) {
-    arr.push(generateRandom(50));
+    arr.push(generateRandom(1, 50));
   }
   renderResponse(
     `<div class="victory">[Позитивный массив] ${arr.join(" | ")} </div>`,
@@ -23,7 +29,7 @@ function fillArrayWithPositive(arr) {
 
 function fillArrayWithNegative(arr) {
   for (let i = 0; i < 10; i++) {
-    arr[i] = generateRandom(-50);
+    arr[i] = generateRandom(-50, -1);
   }
   renderResponse(
     `<div class="victory">[Негативный массив] ${arr.join(" | ")} </div>`,
@@ -32,23 +38,10 @@ function fillArrayWithNegative(arr) {
 
 function fillArrayWithZero(arr) {
   for (let i = 0; i < 10; i++) {
-    arr[i] = generateRandom(0);
+    arr[i] = generateRandom(0, 0);
   }
   renderResponse(
     `<div class="victory">[Нулевой массив] ${arr.join(" | ")} </div>`,
-  );
-}
-
-function fillArrayWithEven(arr) {
-  for (let i = 0; i < 10; i++) {
-    let temp = generateRandom(50);
-    while (temp === 0 && temp % 2 !== 0) {
-      temp = generateRandom(50);
-    }
-    arr[i] = temp;
-  }
-  renderResponse(
-    `<div class="victory">[Четный массив] ${arr.join(" | ")} </div>`,
   );
 }
 
@@ -67,4 +60,5 @@ findAvg(arrayWithRandom);
 fillArrayWithNegative(arrayWithRandom);
 findAvg(arrayWithRandom);
 fillArrayWithZero(arrayWithRandom);
-fillArrayWithEven(arrayWithRandom);
+complexArray(arrayWithRandom);
+// fillArrayWithEven(arrayWithRandom);
---
+++ 5teorem/teorems.js
@@ -0,0 +1,30 @@
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+//Теорема 9.1
+let myNumber = 10;
+
+//Теорема 9.2
+myNumber = randomNumber(-10, 20);
+
+//Теорема 9.3
+let secondNumber = myNumber * 2;
+
+//Теорема 9.4
+secondNumber % 2 === 0 ? (secondNumber = 0) : (secondNumber = 1);
+
+//Теорема 9.5
+if (secondNumber === 0) console.log(`Вторая переменная равна: ${secondNumber}`);
+
+//Теорема 9.6 и 9.7
+while (secondNumber < 10) {
+  console.log(
+    `Сейчас вторая переменная равна: ${secondNumber}, продолжается работа цикла`,
+  );
+  secondNumber += 3;
+}
+
+console.log(
+  `Сейчас вторая переменная равна: ${secondNumber} и мы вышли из цикла!`,
+);
